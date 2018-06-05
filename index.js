86b1d8f5236bee86c7a6fdc0c69101d203a7c447
Matthew Cummings <mfcummin@iu.edu>
2018-06-05 14:10:49 -0400

    Add second input that reads next to spell name

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const changeHeading = function(ev){
 
     const f = ev.target
     const spellName = f.spellName.value
-
+    const powerLevel = f.powerLevel.value
     const spellsDiv = document.querySelector('#spells')
-    spellsDiv.innerHTML += `<li>${spellName}</li>`
+    spellsDiv.innerHTML += `<li>${spellName +' ' +powerLevel}</li>`
+    //'<p>' +spellName + ' ' + powerLevel +'</p>'
+
 
     f.reset()
 }
