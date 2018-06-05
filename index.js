ea372f4ae320c3ff13bd1e4c4378bc66641fd4f1
Matthew Cummings <mfcummin@iu.edu>
2018-06-05 13:40:55 -0400

    update the heading with text from the form input

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-console.log("testing")
-// prints in the console when you inspect
-// .warn for a warning 
-const button = document.querySelector('button')
-const NewSpell = document.querySelector('#Change')
-const sayContents = function(){
-    const heading = document.querySelector('#Title1')
-    heading.textContent = 'Super Secret Spells'
+const form = document.querySelector('form')
 
-    const heading2 = document.querySelector('#Spell1')
-    heading2.textContent = 'Obliviate'
+const changeHeading = function(ev){
+    ev.preventDefault()
 
-    const heading3 = document.querySelector('#Spell2')
-    heading3.textContent = document.querySelector('#Change').value
+    const f = ev.target
+    const spellName = f.spellName.value
+
+    document.querySelector('h1').textContent = spellName
 }
 
-button.addEventListener('click', sayContents)
-NewSpell.addEventListener('keyup', function(event){
-    if(event.keyCode === 13){
-        button.click()
-    }
-})
+form.addEventListener('submit', changeHeading)
