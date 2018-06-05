28ef3785a229ca74cf90b5084649e82b89fdb196
Matthew Cummings <mfcummin@iu.edu>
2018-06-05 13:47:49 -0400

    Use ES6 template literal

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const changeHeading = function(ev){
     const spellName = f.spellName.value
 
     const spellsDiv = document.querySelector('#spells')
-    spellsDiv.textContent += '<p>' + spellName + '</p>'
+    spellsDiv.innerHTML += `<p>${spellName}</p>`
 
     f.reset()
 }
