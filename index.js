a420bb255396df81f7cb7d858f2d6203e355b5fa
Matthew Cummings <mfcummin@iu.edu>
2018-06-08 00:38:55 -0400

    added class word

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-const app = {
-  init: function() {
+class App {
+  constructor() {
     this.spells = []
     this.template = document.querySelector('.spell.template')
+    this.list = document.querySelector('#spells')
+
+    this.load()
 
     const form = document.querySelector('form')
     form.addEventListener('submit', ev => {
       this.handleSubmit(ev)
     })
-  },
+  }
 
-  renderProperty: function(name, value) {
+  load() {
+    // Read the JSON from localStorage
+    const spellJSON = localStorage.getItem('spells')
+
+    // Convert the JSON back into an array
+    const spellArray = JSON.parse(spellJSON)
+
+    // Load the spells back into the app
+    if (spellArray) {
+      spellArray.forEach(this.addSpell.bind(this))
+    }
+  }
+
+  renderProperty(name, value) {
     const el = document.createElement('span')
     el.textContent = value
     el.classList.add(name)
     el.setAttribute('title', value)
     return el
-  },
+  }
 
-  renderItem: function(spell) {
+  renderItem(spell) {
     const item = this.template.cloneNode(true)
     item.classList.remove('template')
 
     // ['name', 'level', etc.]
-    properties = Object.keys(spell)
+    const properties = Object.keys(spell)
 
     // Replace the appropriate values in each <span>
     properties.forEach(property => {
@@ -33,6 +49,11 @@ const app = {
       }
     })
 
+    // Mark it as a favorite, if applicable
+    if (spell.favorite) {
+      item.classList.add('fav')
+    }
+
     // delete button
     item
       .querySelector('button.delete')
@@ -41,17 +62,7 @@ const app = {
         this.removeSpell.bind(this, spell)
       )
 
-    // const favButton = function(){
-    //   const fav = document.que
-    // }  
-    // // fav button
-    // // const favButton = function(){
-    // //   const fav = document.querySelector('button.fav')
-    // //   fav.createElement('span')
-    // //   fav.classList.add(favorite)
-    
-    // // }
-    // addEventListener('fav', favButton)
+    // fav button
     item
       .querySelector('button.fav')
       .addEventListener(
@@ -59,10 +70,70 @@ const app = {
         this.toggleFavorite.bind(this, spell)
       )
 
-    return item
-  },
+    // move up
+    item
+      .querySelector('button.up')
+      .addEventListener(
+        'click',
+        this.moveUp.bind(this, spell)
+      )
 
-  removeSpell: function(spell, ev) {
+    // move down
+    item
+      .querySelector('button.down')
+      .addEventListener(
+        'click',
+        this.moveDown.bind(this, spell)
+      )
+
+    return item
+  }
+
+  moveDown(spell, ev) {
+    // Find the <li>
+    const button = ev.target
+    const item = button.closest('.spell')
+
+    // Find its index in the array
+    const i = this.spells.indexOf(spell)
+
+    // Only move it if it's not already last
+    if (i < this.spells.length - 1) {
+      // Move it in the array
+      const nextSpell = this.spells[i + 1]
+      this.spells[i + 1] = spell
+      this.spells[i] = nextSpell
+
+      // Move it on the page
+      this.list.insertBefore(item.nextSibling, item)
+    }
+
+    this.save()
+  }
+
+  moveUp(spell, ev) {
+    // Find the <li>
+    const button = ev.target
+    const item = button.closest('.spell')
+
+    // Find its index in the array
+    const i = this.spells.indexOf(spell)
+
+    // Only move it if it's not already first
+    if (i > 0) {
+      // Move it in the array
+      const previousSpell = this.spells[i - 1]
+      this.spells[i - 1] = spell
+      this.spells[i] = previousSpell
+
+      // Move it on the page
+      this.list.insertBefore(item, item.previousSibling)
+    }
+
+    this.save()
+  }
+
+  removeSpell(spell, ev) {
     // Remove from the DOM
     const button = ev.target
     const item = button.closest('.spell')
@@ -71,13 +142,25 @@ const app = {
     // Remove from the array
     const i = this.spells.indexOf(spell)
     this.spells.splice(i, 1)
-  },
 
-  toggleFavorite: function(spell, ev) {
-    spell.fav = !spell.fav
-  },
+    this.save()
+  }
 
-  handleSubmit: function(ev) {
+  toggleFavorite(spell, ev) {
+    const button = ev.target
+    const item = button.closest('.spell')
+    spell.favorite = item.classList.toggle('fav')
+    this.save()
+  }
+
+  addSpell(spell) {
+    this.spells.push(spell)
+
+    const item = this.renderItem(spell)
+    this.list.appendChild(item)
+  }
+
+  handleSubmit(ev) {
     ev.preventDefault()
 
     const f = ev.target
@@ -85,18 +168,22 @@ const app = {
     const spell = {
       name: f.spellName.value,
       level: f.level.value,
-      fave: false,
+      favorite: false,
     }
-    this.spells.push(spell)
 
-    const item = this.renderItem(spell)
+    this.addSpell(spell)
 
-    const list = document.querySelector('#spells')
-    list.appendChild(item)
-
+    this.save()
     f.reset()
     f.spellName.focus()
-  },
+  }
+
+  save() {
+    localStorage.setItem(
+      'spells',
+      JSON.stringify(this.spells)
+    )
+  }
 }
 
-app.init()
+const app = new App()
